a528f5eaee28d1131b60f4f36d8ed94fd7902011
byh1000 <byh1000@wips.co.kr>
2018-02-21 11:00:37 +0900

    test 2

--- pyImage.py
+++ pyImage.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     print(result)
     print(sys.argv)
 
-    ssss112233445566667777 = sys.argv
+    ssss112233445566667777888 = sys.argv
 
 #for i in s.split(','):
 #    print (i+ " "+sys.argv[i])
